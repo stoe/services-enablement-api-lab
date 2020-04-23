a0a5605650a9e3e7f4bb35dbd481b49b29dc0e97
Stefan Stölzle <stoe@github.com>
2020-04-23 19:13:33 +0200

    📦 Add local action

---
+++ .github/actions/hello-world/index.js
@@ -0,0 +1,18 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+async function run() {
+  try {
+    const token = core.getInput('repo-token', {required: true})
+    const client = new github.GitHub(token)
+
+    const {context} = github
+
+    const issueComment = context.issue({body: 'Hello from GitHub Actions!'})
+    return client.issues.createComment(issueComment)
+  } catch (err) {
+    core.setFailed(err.message)
+  }
+}
+
+run()
